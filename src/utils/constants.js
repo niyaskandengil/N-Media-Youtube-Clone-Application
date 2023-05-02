119c7e56ef079735cb8a937de26b838e3c29c035
Niyas Kandengil <niyaskandengil@gmail.com>
2023-05-02 11:48:24 +0530

    first comnt

---
+++ src/utils/constants.js
@@ -0,0 +1,40 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+
+export const logo = 'https://i.ibb.co/rknPk97/favicosn-3.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'NRK_X', icon: <SportsEsportsIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://ibb.co/SvXZZR6';
+export const demoChannelUrl = '/channel/UCfBvhlRfmbMdReC4PgSGyIg';
+export const demoVideoUrl = '/video/wLfH_ZCR4GA';
+export const demoChannelTitle = 'NRK_X';
+export const demoVideoTitle = 'Sound? - JETT - Valorant';
+export const demoProfilePicture = 'https://ibb.co/L8YWQsV'
