5b94e0a4b473095db04d37ff5edf951ca9dc51f3
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 01:14:52 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,8 +12,9 @@ import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
+// export const logo = 'https://i.ibb.co/rknPk97/favicosn-3.png';
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
-export const logo = 'https://i.ibb.co/rknPk97/favicosn-3.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
